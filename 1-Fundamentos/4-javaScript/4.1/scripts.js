f8676b9f2afb0a6fe27a4b2994cf70279774a8a8
rafael rocha <1rafaelrocha2@gmail.com>
2022-03-07 16:02:01 -0300

    js

---
+++ 1-Fundamentos/4-javaScript/4.1/scripts.js
@@ -0,0 +1,15 @@
+let processo = "reprovada";
+
+switch (processo){
+  case  "aprovada":
+    console.log("Você foi aprovado(a)");
+    break;
+
+  case "lista":
+    console.log("Você está na lista de espera!");
+    break;
+
+  case "reprovada":
+    console.log("Você foi reprovado(a)!");
+    break;
+}
