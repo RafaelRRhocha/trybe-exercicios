d1b6df4ca4d0579f9285f12ed729bbed9049b06c
rafael rocha <1rafaelrocha2@gmail.com>
2022-03-08 15:45:48 -0300

    update

---
+++ 1-Fundamentos/4-javaScript/4.2/scripts.js
@@ -0,0 +1,11 @@
+// let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+// for (let i = 0; i < groceryList.length; i++) {
+//      console.log(groceryList[i]);
+// }
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for (name of names) {
+    console.log(name);
+}
