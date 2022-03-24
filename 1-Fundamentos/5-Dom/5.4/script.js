4f53af05c3cec59c498d8ba7a5c8c3459d6fddcb
rafael rocha <1rafaelrocha2@gmail.com>
2022-03-24 20:35:21 -0300

    update 5.1

---
+++ 1-Fundamentos/5-Dom/5.4/script.js
@@ -0,0 +1,6 @@
+//01
+function trocarcor() {
+    let Cor =  document.querySelector('#bgcolor').value;
+    document.getElementById('meufundo').style.backgroundColor = Cor;
+}
+
---
+++ 1-Fundamentos/6-Forms/5.1/script.js
@@ -0,0 +1,4 @@
+const pegarBtn = document.querySelector('#btn-apagar');
+pegarBtn.addEventListener('click', () => {
+  const pegarInput = document.querySelectorAll('input');
+});
