da951b11897b5277e3e3dc5562410d12fb35e107
rafael rocha <1rafaelrocha2@gmail.com>
2022-04-01 18:43:30 -0300

    update d-1

---
+++ 1-Fundamentos/7-javaScriptES6/7.1/script.js
@@ -0,0 +1,20 @@
+//Ex 1
+// const testarScopo = () => {
+//     const escopo = true;
+//     if (escopo === true) {
+//       var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+//       ifScope = `${ifScope}   ótimo, fui utilizada no escopo !`;
+//       console.log(ifScope);
+//     } else {
+//       var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+//       console.log(elseScope);
+//     }
+//     console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+//   }
+
+//Ex 2
+// const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+// const arr = oddsAndEvens.sort((a, b) => {return a - b});
+
+// console.log(`${arr} se encontram ordenados de forma crescente!`);
