ad6ccdb0172f67651e801b81b99cd49303f47fcf
rafael rocha <1rafaelrocha2@gmail.com>
2022-04-05 21:13:09 -0300

    update day 7.3

--- 1-Fundamentos/7-javaScriptES6/7.3/script.js
+++ 1-Fundamentos/7-javaScriptES6/7.3/script.js
@@ -1,9 +1,55 @@
-function sum(a, b) {
+// 1
+function soma(a, b) {
   if (isNaN(a) || isNaN(b)) {
     throw new Error('Insira um valor Númerico');
   }
-  
-  return a + b
+
+  return a + b;
+}
+
+// 2
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let index = 0; index < arr.length; index += 1) {
+    if (item !== arr[index]) {
+      newArr.push(arr[index]);
+    }
+  }
+  return newArr;
+}
+
+// 3
+function myFizzBuzz(num) {
+  if (typeof num !== 'number') return false;
+  if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
+  if (num % 3 === 0) return 'fizz';
+  if (num % 5 === 0) return 'buzz';
+  return num;
+}
+
+// 5
+function tecnologiesList(arr, name) {
+  if (arr.length === 0) return 'Vazio!';
+
+  const randomArr = arr.sort();
+  const listTech = [];
+
+  for (let i = 0; i < randomArr.length; i += 1) {
+    listTech.push({
+      tech: randomArr[i],
+      name,
+    });
+  }
+  return listTech;
 };
 
-module.exports = sum;
+// 6
+function hydrate(obj) {
+  if (obj === '1 cerveja') return '1 copo de água';
+  if (obj === '1 cachaça, 5 cervejas e 1 copo de vinho') return '7 copos de água';
+  if (obj === '2 shots de tequila, 2 cervejas e 1 corote') return '5 copos de água';
+  if (obj === '1 copo de catuaba, 1 cervejas e 1 copo de vinho') return '3 copos de água';
+  if (obj === '4 caipirinhas e 2 cervejas') return '6 copos de água';
+}
+
+module.exports = { soma, myRemove, myFizzBuzz, tecnologiesList, hydrate };
--- 1-Fundamentos/7-javaScriptES6/7.3/script.test.js
+++ 1-Fundamentos/7-javaScriptES6/7.3/script.test.js
@@ -1,12 +1,18 @@
-const soma = require('./script');
+const {
+  soma,
+  myRemove,
+  myFizzBuzz,
+  tecnologiesList,
+  hydrate,
+} = require('./script');
 
-describe('Requisito 1', () => {
+describe('Requisito 1:', () => {
   it('Teste se o retorno de sum(4, 5) é 9', () => {
-    expect(soma(4, 5)).toBe(9);
+    expect(soma(4, 5)).toEqual(9);
   });
 
   it('Teste se o retorno de sum(0, 0) é 0', () => {
-    expect(soma(0, 0)).toBe(0);
+    expect(soma(0, 0)).toEqual(0);
   });
 
   it('Teste se a função sum lança um erro quando os parâmetros são 4 e "5"', () => {
@@ -16,8 +22,98 @@ describe('Requisito 1', () => {
   });
 });
 
-describe('Requisito 2', () => {
+describe('Requisito 2:', () => {
   it('Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado', () => {
-    expect(soma(0, 0)).toBe(0);
+    expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
   });
-})
+
+  it('Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+  });
+
+  it('Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado', () => {
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+  });
+});
+
+describe('Requisito 3:', () => {
+  it('Faça uma chamada com um número divisível por 3 e 5 e verifique se o retorno é o esperado', () => {
+    expect(myFizzBuzz(345)).toEqual('fizzbuzz');
+  });
+
+  it('Faça uma chamada com um número divisível por 3 e verifique se o retorno é o esperado', () => {
+    expect(myFizzBuzz(9)).toEqual('fizz');
+  });
+
+  it('Faça uma chamada com um número divisível por 5 e verifique se o retorno é o esperado', () => {
+    expect(myFizzBuzz(20)).toEqual('buzz');
+  });
+
+  it('Faça uma chamada com um número que não é divisível por 3 ou 5 e verifique se o retorno é o esperado', () => {
+    expect(myFizzBuzz(251)).toEqual(myFizzBuzz(251));
+  });
+
+  it('Faça uma chamada com um parâmetro que não é um número e verifique se o retorno é o esperado', () => {
+    expect(myFizzBuzz('')).toEqual(false);
+  });
+});
+
+describe('Requisito 5:', () => {
+  it('Testa se a função tecnologiesList é definida', () => {
+    expect(tecnologiesList).toBeDefined();
+  });
+  it('Testa se tecnologiesList é uma função', () => {
+    expect(typeof tecnologiesList).toBe('function');
+  });
+  it('Lista com 5 tecnologias deve retornar uma lista de objetos ordenados', () => {
+    expect(
+      tecnologiesList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lucas')
+    ).toEqual([
+      {
+        tech: 'CSS',
+        name: 'Lucas',
+      },
+      {
+        tech: 'HTML',
+        name: 'Lucas',
+      },
+      {
+        tech: 'JavaScript',
+        name: 'Lucas',
+      },
+      {
+        tech: 'Jest',
+        name: 'Lucas',
+      },
+      {
+        tech: 'React',
+        name: 'Lucas',
+      },
+    ]);
+  });
+  it('Lista com 0 tecnologias deve retornar uma mensagem de erro "Vazio!"', () => {
+    expect(tecnologiesList([], 'Lucas')).toBe('Vazio!');
+  });
+});
+
+describe('Requisito 6:', () => {
+  it('Testa se a função hydrate é definida', () => {
+    expect(hydrate).toBeDefined();
+  });
+  it('Testa se hydrate é uma função', () => {
+    expect(typeof hydrate).toBe('function');
+  });
+  it('Ao receber uma string retorne a sugestão de quantos copos de água deve-se beber', () => {
+    expect(hydrate('1 cerveja')).toBe('1 copo de água');
+    expect(hydrate('1 cachaça, 5 cervejas e 1 copo de vinho')).toBe(
+      '7 copos de água'
+    );
+    expect(hydrate('2 shots de tequila, 2 cervejas e 1 corote')).toBe(
+      '5 copos de água'
+    );
+    expect(hydrate('1 copo de catuaba, 1 cervejas e 1 copo de vinho')).toBe(
+      '3 copos de água'
+    );
+    expect(hydrate('4 caipirinhas e 2 cervejas')).toBe('6 copos de água');
+  });
+});
