17a8f8cb1d3bafddc0c2776ffe86e1954469856c
rafael rocha <1rafaelrocha2@gmail.com>
2022-04-11 09:24:00 -0300

    forEach

---
+++ 1-Fundamentos/8-HighOrderFunctions/8.2/script.js
@@ -0,0 +1,8 @@
+// forEach
+const emailListInData = [
+  'roberta@email.com',
+  'paulo@email.com',
+  'anaroberta@email.com',
+  'fabiano@email.com',
+];
+emailListInData.forEach((email) => console.log(`O email ${email} esta cadastrado em nosso banco de dados!`));
