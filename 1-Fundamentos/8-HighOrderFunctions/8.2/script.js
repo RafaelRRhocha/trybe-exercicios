49e48ebfdd485c507f96dca491a4afb5e607d288
rafael rocha <1rafaelrocha2@gmail.com>
2022-04-11 09:42:50 -0300

    find

--- 1-Fundamentos/8-HighOrderFunctions/8.2/script.js
+++ 1-Fundamentos/8-HighOrderFunctions/8.2/script.js
@@ -6,3 +6,27 @@ const emailListInData = [
   'fabiano@email.com',
 ];
 emailListInData.forEach((email) => console.log(`O email ${email} esta cadastrado em nosso banco de dados!`));
+
+// Find
+const numbers = [19, 21, 30, 3, 45, 22, 15];
+const verificar15 = (number) => number % 15 === 0;
+const log = numbers.find(verificar15);
+console.log(log);
+
+
+const names = ['João', 'Irene', 'Fernando', 'Maria'];
+const findNameWithFiveLetters = () => {
+  return names.find((name) => name.length <= 4);
+}
+console.log(findNameWithFiveLetters());
+
+const musicas = [
+  { id: '31031685', title: 'Partita in C moll BWV 997' },
+  { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
+  { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+]
+
+function findMusic(id) {
+  return musicas.find((musica) => musica.id === id);
+}
+console.log(findMusic('31031685'))
