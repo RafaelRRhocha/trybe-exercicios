1d184a27acbbbd3210326fb2e8629b61b072461e
rafael rocha <1rafaelrocha2@gmail.com>
2022-04-11 09:49:38 -0300

    up every

--- 1-Fundamentos/8-HighOrderFunctions/8.2/script.js
+++ 1-Fundamentos/8-HighOrderFunctions/8.2/script.js
@@ -38,4 +38,19 @@ const hasName = (arr, name) => {
 }
 console.log(hasName(names, 'Ana'));
 
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+const verifyAges = (arr, minimumAge) => {
+  return arr.every((element) => element >= minimumAge)
+}
+console.log(verifyAges(people, 18));
+
+
+
 
