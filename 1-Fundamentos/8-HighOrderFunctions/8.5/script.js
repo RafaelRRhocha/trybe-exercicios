e5dc48060357ecbc06c6765c86f3e8bd87faed0a
rafael rocha <1rafaelrocha2@gmail.com>
2022-04-13 15:12:00 -0300

    update testes

---
+++ 1-Fundamentos/8-HighOrderFunctions/8.5/script.js
@@ -0,0 +1,25 @@
+// const user = {
+//   name: 'Maria',
+//   age: 21,
+//   nationality: 'Brazilian',
+// };
+
+// const jobInfos = {
+//   profession: 'Software engineer',
+//   squad: 'Rocket Landing Logic',
+//   squadInitials: 'RLL',
+// };
+
+// const { name: nome, age: idade, nationality: nacionalidade } = user;
+// const {
+//   profession: profissao,
+//   squad: empresa,
+//   squadInitials: iniciais,
+// } = jobInfos;
+// const infos = {
+//   ...user,
+//   ...jobInfos,
+// };
+
+// console.log(`OLá! meu nome é ${nome}, tenho ${idade} e sou ${nacionalidade}
+// Eu trabalho como ${profissao}, na empresa ${empresa} - ${iniciais}`);
