a5fca1ced535c8a4c75a9643c92151c2f8cf92c3
rafael rocha <1rafaelrocha2@gmail.com>
2022-04-13 19:30:10 -0300

    requisito 8

--- 1-Fundamentos/8-HighOrderFunctions/8.5/exercícios.js
+++ 1-Fundamentos/8-HighOrderFunctions/8.5/exercícios.js
@@ -107,8 +107,15 @@ const ships = [
   },
 ];
 
-const shipLength = () => {}
+const shipLength = ({name, length, measurementUnit}) => `${name} is ${length} ${measurementUnit} long`;
 
 console.log(shipLength(ships[0])); // 'Titanic is 269.1 meters long'
 console.log(shipLength(ships[1])); // 'Queen Mary 2 is 1132 feet long'
 console.log(shipLength(ships[2])); // 'Yamato is 256 meters long'
+
+// 08
+const greet = () => 
+
+console.log(greet('John')) // 'Hi John'
+console.log(greet('John', 'Good morning')) // 'Good morning John'
+console.log(greet('Isabela', 'Oi')) // 'Oi Isabela'
