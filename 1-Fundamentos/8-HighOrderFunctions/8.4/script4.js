30dce0318679fff2932355759d84cda9d1dde724
rafael rocha <1rafaelrocha2@gmail.com>
2022-04-12 21:31:07 -0300

    update 6

--- 1-Fundamentos/8-HighOrderFunctions/8.4/script4.js
+++ 1-Fundamentos/8-HighOrderFunctions/8.4/script4.js
@@ -1,9 +1,15 @@
 const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
-const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
+const grades = [
+  [9, 8, 10, 7, 5],
+  [10, 9, 9, 10, 8],
+  [10, 7, 10, 8, 9],
+];
 
-function studentAverage() {
-  
-}
+const alunos = students.map((nome, i) => ({
+  name: nome,
+  nota: grades[i].reduce((acc, nota) => acc + nota, 0) / grades[i].length,
+}));
+console.log(alunos);
 
 const expected = [
   { name: 'Pedro Henrique', average: 7.8 },
