12f04470993155d32fdf59d940188941f3389dcf
rafael rocha <1rafaelrocha2@gmail.com>
2022-04-14 21:23:21 -0300

    update arr

--- 1-Fundamentos/8-HighOrderFunctions/8.4/script4.js
+++ 1-Fundamentos/8-HighOrderFunctions/8.4/script4.js
@@ -2,7 +2,7 @@ const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
 const grades = [
   [9, 8, 10, 7, 5, 4],
   [10, 9, 9, 10, 8, 2],
-  [10, 7, 10, 8, 9],
+  [10, 7, 10, 8, 9, 1],
 ];
 
 const alunos = students.map((nome, i) => ({
