82fd203d0a24c392886ca3741c0431366e6d0b8a
rafael rocha <1rafaelrocha2@gmail.com>
2022-05-02 15:01:16 -0300

    mod app

--- 2-FrontEnd/10-fundamentosReact/testando-meu-computador/src/App.js
+++ 2-FrontEnd/10-fundamentosReact/testando-meu-computador/src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+const textJSX = "Hello, JSX"
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {textJSX}
         </p>
         <a
           className="App-link"
