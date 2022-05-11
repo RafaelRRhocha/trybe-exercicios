e51221452bb031fbf372b281213c09fa2770df08
rafael rocha <1rafaelrocha2@gmail.com>
2022-05-11 19:45:47 -0300

    up btn

--- 2-FrontEnd/11-componentesComEstado/11.2/myForms/src/Button.jsx
+++ 2-FrontEnd/11-componentesComEstado/11.2/myForms/src/Button.jsx
@@ -4,7 +4,7 @@ export class Button extends Component {
   render() {
     return (
       <div className="btns">
-        <button className="btn" type="submit">Enviar</button>
+        <button className="btn" onClick={() => alert('Formulário enviado!')} type="submit">Enviar</button>
         <button className="btn" type="reset">Limpar</button>
       </div>
     )
