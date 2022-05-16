339153439dda63a8ea3ba2129c7ee3a7866a0cf4
rafael rocha <1rafaelrocha2@gmail.com>
2022-05-16 19:09:26 -0300

    ex12

--- 2-FrontEnd/12-cicloDeVidaEmReact/12.1/ex12.1/src/App.jsx
+++ 2-FrontEnd/12-cicloDeVidaEmReact/12.1/ex12.1/src/App.jsx
@@ -1,7 +1,37 @@
-import './App.css'
+import './App.css';
+import React from 'react';
 
-function App() {
-  return ( null )
+export default class App extends React.Component {
+  state = {
+    dog: '',
+    loading: true,
+  }
+
+  componentDidMount() {
+  this.randomDogImages()
+  }
+
+  randomDogImages = () => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          dog: data.message,
+          loading: false,
+        })
+    })
+  }
+
+  render() {
+    const { dog, loading} = this.state
+    return (
+      <>
+        {
+          loading ? (<p>Loading...</p>) :
+          <img src={ dog } alt="imagem de um dog" width="200px" ></img>
+        }
+        <button type="button" onClick={ this.randomDogImages }>ProxDog</button>
+      </>
+    )
+  }
 }
-
-export default App
