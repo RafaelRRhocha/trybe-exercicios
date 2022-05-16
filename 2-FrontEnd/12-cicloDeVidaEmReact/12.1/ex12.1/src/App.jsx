5d4e3aea793156b2d5e88c966056bf65127e06a8
rafael rocha <1rafaelrocha2@gmail.com>
2022-05-16 19:26:43 -0300

    push app

--- 2-FrontEnd/12-cicloDeVidaEmReact/12.1/ex12.1/src/App.jsx
+++ 2-FrontEnd/12-cicloDeVidaEmReact/12.1/ex12.1/src/App.jsx
@@ -25,13 +25,13 @@ export default class App extends React.Component {
   render() {
     const { dog, loading} = this.state
     return (
-      <>
+      <div className="flex flex-col justify-center">
         {
           loading ? (<p>Loading...</p>) :
           <img src={ dog } alt="imagem de um dog" width="200px" ></img>
         }
-        <button type="button" onClick={ this.randomDogImages }>ProxDog</button>
-      </>
+        <button type="button" onClick={ this.randomDogImages } class="inline-flex justify-center py-2 w-[100px] px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">next Dog</button>
+      </div>
     )
   }
 }
