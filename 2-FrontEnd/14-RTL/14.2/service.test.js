7dd72e43dafbe99217db6721f34bf5c3c8e1b686
Rafael Rocha <1rafaelrocha2@gmail.com>
2022-06-02 19:33:22 -0300

    p2

--- 2-FrontEnd/14-RTL/14.2/service.test.js
+++ 2-FrontEnd/14-RTL/14.2/service.test.js
@@ -1,13 +1,17 @@
 let { services } = require('./service');
 
 describe('teste se o teste funciona', () => {
-  services = jest.fn().mockReturnValue(3);
-  services();
   it('testando se a função foi chamada', () => {
+    services = jest.fn().mockReturnValue(3);
+
     expect(services()).toEqual(3);
   });
 
   it('Teste se a função foi chamada e a nova implementação de divisão foi aplicada.', () => {
+    services = jest.fn().mockImplementationOnce((a, b) => a / b);
+
+    expect(services(20, 2)).toEqual(10);
     expect(services).toHaveBeenCalled();
+    expect(services(20, 2)).not.toEqual(10);
   });
 });
