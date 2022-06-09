3d9df132fccb0fde5769fa3396e846b391fb8474
Rafael Rocha <1rafaelrocha2@gmail.com>
2022-06-09 12:21:10 -0300

    finish install redux packages

--- 2-FrontEnd/15-Redux/15.2/ex15.2/src/App.jsx
+++ 2-FrontEnd/15-Redux/15.2/ex15.2/src/App.jsx
@@ -1,7 +1,10 @@
-import './App.css'
+import React from 'react';
+import './App.css';
 
 function App() {
-  return (<h1>Hello, World</h1>);
+  return (
+    <h1>Hello, World</h1>
+  )
 }
 
 export default App
