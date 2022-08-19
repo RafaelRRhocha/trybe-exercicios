65898f8e141a874d6cb232957c44336a6a7d45da
Rafael <1rafaelrocha2@gmail.com>
2022-08-19 19:31:20 -0300

    resume all

--- 3-BackEnd/b-22/personagens.js
+++ 3-BackEnd/b-22/personagens.js
@@ -1,15 +1,29 @@
 const fs = require('fs').promises;
+const readline = require('readline-sync');
 
 const fetch = async () => {
   const request = await fs.readFile('./simpsons.json', 'utf-8');
   return JSON.parse(request);
 }
 
-const Main = async () => {
+const filter = async () => {
   const api = await fetch();
 
   const idName = api.map(({id, name}) => `id: ${id}, name: ${name}`)
   console.log(idName);
+
+  const idUser = readline.questionInt('digite o seu id: ');
+  return api.filter(({ id }) => {
+    if(idUser === id) {
+      return console.log('id compativel')
+    } else {
+      return console.log('id não compativel')
+    }
+  })
+}
+
+const Main = async () => {
+  await filter()
 }
 
 Main();
