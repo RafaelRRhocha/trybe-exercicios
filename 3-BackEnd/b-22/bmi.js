343c5cd00bc0f53d6ba16765c3aab8ea700658ce
Rafael <1rafaelrocha2@gmail.com>
2022-08-19 18:43:27 -0300

    ex 3 finalizado

--- 3-BackEnd/b-22/bmi.js
+++ 3-BackEnd/b-22/bmi.js
@@ -1,3 +1,12 @@
-const IMC = (peso, altura) => console.log(Math.round(peso / (altura ** 2)));
+const readline = require('readline-sync');
 
-console.log(IMC(90, 1.87));
+const IMC = (peso, altura) => console.log(Math.floor(peso / ((altura / 100) ** 2)));
+
+const Main = () => {
+  const peso = readline.questionInt('qual o seu peso?');
+  const altura = readline.questionInt('qual a sua altura?');
+  const BMI = IMC(peso, altura);
+  return BMI;
+}
+
+Main();
