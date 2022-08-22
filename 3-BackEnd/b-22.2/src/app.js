38ccf0bc44cd93af2779e96d76d7260991ac400f
Rafael <1rafaelrocha2@gmail.com>
2022-08-22 16:13:24 -0300

    req 4 finalizado

--- 3-BackEnd/b-22.2/src/app.js
+++ 3-BackEnd/b-22.2/src/app.js
@@ -2,4 +2,27 @@ const express = require('express');
 
 const app = express();
 
+const activities = [
+  {
+    id: 1,
+    description: 'Banho no cachorro',
+    status: 'A fazer',
+  },
+  {
+    id: 2,
+    description: 'Cortar a grama',
+    status: 'A fazer',
+  },
+  {
+    id: 3,
+    description: 'Estudar JavaScript',
+    status: 'Feito',
+  },
+];
+
+app.get('/myActivities/:id', (req, res) => {
+  const filterActivities = activities.filter(e => e.id === req.params.id);
+  res.status(200).json(filterActivities);
+});
+
 module.exports = app;
