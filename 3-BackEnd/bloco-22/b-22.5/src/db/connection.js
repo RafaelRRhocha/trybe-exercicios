3c9cf7825a28225b32b2598ad1db972f803f38cd
Rafael <1rafaelrocha2@gmail.com>
2022-08-25 18:36:25 -0300

    ex 1 e 2 finalizados

---
+++ 3-BackEnd/bloco-22/b-22.5/src/db/connection.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql2/promise');
+
+const connection = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE_NAME,
+  connectionLimit: process.env.MYSQL_CONNECTION_LIMIT,
+  queueLimit: process.env.MYSQL_QUEUE_LIMIT,
+});
+
+module.exports = connection; 
