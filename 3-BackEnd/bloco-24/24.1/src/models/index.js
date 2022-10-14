a43c2f4a2ea759fff4595124707d0f3e588e4258
Rafael Rocha <1rafaelrocha2@gmail.com>
2022-10-14 18:03:13 -0300

    fix sequelize fs

--- 3-BackEnd/bloco-24/24.1/src/models/index.js
+++ 3-BackEnd/bloco-24/24.1/src/models/index.js
@@ -15,12 +15,9 @@ if (config.use_env_variable) {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
+fs.readdirSync(__dirname).filter(file => {
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
+  }).forEach(file => {
     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });
