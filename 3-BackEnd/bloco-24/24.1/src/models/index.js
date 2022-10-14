a11c96c8947d630eebab407a3c7849516f9e0e24
Rafael Rocha <1rafaelrocha2@gmail.com>
2022-10-14 18:04:00 -0300

    create new ifs for index

--- 3-BackEnd/bloco-24/24.1/src/models/index.js
+++ 3-BackEnd/bloco-24/24.1/src/models/index.js
@@ -9,11 +9,8 @@ const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+if (config.use_env_variable) sequelize = new Sequelize(process.env[config.use_env_variable], config);
+if (!config.use_env_variable) sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 fs.readdirSync(__dirname).filter(file => {
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
