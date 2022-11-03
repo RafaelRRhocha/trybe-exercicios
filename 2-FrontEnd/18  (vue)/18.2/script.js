b6b5bd36bcd6aea058b2619ec5e594b441b19eb3
Rafael <1rafaelrocha2@gmail.com>
2022-11-03 20:48:18 -0300

    crud

--- 2-FrontEnd/18  (vue)/18.2/script.js
+++ 2-FrontEnd/18  (vue)/18.2/script.js
@@ -17,7 +17,29 @@ const vm = {
       films: teste
     }
   },
-  methods: {}
+  methods: {
+    addMovie() {
+      this.films.push({
+        id: 03,
+        title: 'A hora do rush 2',
+        genre: 'Action, Comedy'
+      })
+    },
+    removeMovie() {
+      this.films.pop()
+    },
+    updateMovie() {
+      this.$set(
+          this.films,
+          02,
+          {
+            id: 02,
+            title: 'Pink Panther 2',
+            genre: 'Comedy, Action'
+          }
+        )
+    }
+  }
 };
 
 Vue.createApp(vm).mount('#app');
